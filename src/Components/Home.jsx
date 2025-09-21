33c9500e6c71ad31daa8d0e23e948d4da3819ed7
suhaoudeh <soudeh@my.centennialcollege.ca>
2025-09-21 14:41:34 -0400

    add Render Text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,9 @@
 // Components/Home.jsx
+import { Link } from "react-router-dom";
 import logo from '../assets/logo1.png';
 export default function Home() {
   return (
-    <>
-    
+       <>
       
       <div className="home-container">
         <img src={logo} alt="Logo" className="logo" />
