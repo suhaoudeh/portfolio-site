8f0e2b175eab3de57e223e4937936f949e46c93d
suhaoudeh <soudeh@my.centennialcollege.ca>
2025-09-21 12:39:57 -0400

    Improve CSS styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
-      
       <p>Feel free to explore the different sections and learn more about me.</p>
       <p>I'm a passionate web developer with experience in building modern web applications.</p>
       <p>Check out my projects and get in touch if you'd like to collaborate!</p>
